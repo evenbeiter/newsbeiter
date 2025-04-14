25e82f4b72e08e0ba0dd1905bbc31d0de46e3b4a
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-04-14 12:07:01 +0800

    Update news.js

--- news.js
+++ news.js
@@ -1268,3 +1268,13 @@ window.onscroll = function () {
     }
   }, 1000);
 };
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(function(reg) {
+      console.log('Service Worker registered:', reg);
+    })
+    .catch(function(err) {
+      console.error('Service Worker registration failed:', err);
+    });
+}
