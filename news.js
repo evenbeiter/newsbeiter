bcfdabefe48b8d9e0c9672f3a127a644d699f659
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-05-04 20:26:37 +0800

    Update news.js

--- news.js
+++ news.js
@@ -1365,8 +1365,8 @@ function cvtS2HHMMSS(sec,rescale) {
 }
 
 function cvt2Timezone(timestamp) {
-    const date = new Date(timestamp);
-    return date.toLocaleString();
+  const date = new Date(timestamp);
+  return date.toLocaleString('zh-TW',{timeZone:'Asia/Taipei'});
 }
 
 function sCC(a,ii){
