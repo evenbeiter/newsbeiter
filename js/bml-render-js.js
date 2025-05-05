801b90d3625212f035e4bfa221176943a81694b6
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-05-05 09:17:42 +0800

    Update bml-render-js.js

--- js/bml-render-js.js
+++ js/bml-render-js.js
@@ -4,18 +4,18 @@ for (let tab of tabs){
 }
 get1stList(siteNameVar,tabs[0][1],tabs[0][0]);
 
-for (let tab of faq){
+for (let tab of faqB){
   searchList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="getFAQSearchResults('${tab[0]}','${tab[1]}','${tab[2]}')">${tab[2]}</button>`;
 }  
 searchList.innerHTML+='<input type="text" id="search-term" class="form-control my-2">';
-for (let tab of searchSites){
+for (let tab of searchSitesB){
   searchList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="get1stSearchResults('${tab[0]}','${tab[1]}')">${tab[1]}</button>`;
 }
 } else {
   openUrlList()
 }
 
-for (let tab of allSites1){
+for (let tab of allSitesB){
   urlList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="openUrl('${tab[4]}')">${tab[1]}</button>`;
 }
 urlList.innerHTML+='<hr>';
