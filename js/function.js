1ac3903dfcc841deaa43fb66d835f55ebb328243
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-05-05 13:32:09 +0800

    Update function.js

--- js/function.js
+++ js/function.js
@@ -70,9 +70,9 @@ async function ping(){
 function createChannelList(site,siteName,top){
   channelList.innerHTML='';
   for (let tab of site){
-    channelList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="get1stList('${siteName}','${top}','${tab[0]}')">${tab[1]}</button>`;
+    channelList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="get1stList('${siteName}','${tab[1]}','${tab[0]}')">${tab[1]}</button>`;
   }
-  get1stList(siteName,top,site[0][0]);
+  get1stList(siteName,site[0][1],site[0][0]);
   openChannelList();
 }
 
