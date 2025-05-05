d131ef8ce93f04c043557e6c3d818d249d990c30
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-05-05 16:58:28 +0800

    Update function.js

--- js/function.js
+++ js/function.js
@@ -73,7 +73,7 @@ function createChannelList(site,siteName,top){
     channelList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="get1stList('${siteName}','【${top}】${tab[1]}','${tab[0]}')">${tab[1]}</button>`;
   }
   openChannelList();
-  get1stList(siteName,site[0][1],site[0][0]);
+  get1stList(siteName,'【'+top+'】'+site[0][1],site[0][0]);
 }
 
 function createSearchListDiv(faqList,searchSiteList){
