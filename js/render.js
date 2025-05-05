7f6e3c7a3739fbd4fe2611c07fe6df0bb9962412
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-05-05 18:54:48 +0800

    Update render.js

--- js/render.js
+++ js/render.js
@@ -274,7 +274,7 @@ var loading=document.getElementById('loading');
 
 if (window.location.href.indexOf('evenbeiter.github.io')==-1){
   if (siteNameVar!==''){
-    createChannelList(tabs,siteNameVar,tabs[0][1])
+    createChannelList(tabs,siteNameVar,'【'+docTitle+'】'+tabs[0][1])
     createSearchListDiv(faqB,searchSitesB);
     createUrlListDiv4Bml([allSitesB,otherVideoB]);
   } else {
