416abd40ecef7e8615c33c22654b8136c0c625f3
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-05-05 11:38:35 +0800

    Update bml-render-js.js

--- js/bml-render-js.js
+++ js/bml-render-js.js
@@ -1,24 +1,40 @@
-if (siteNameVar!==''){
-for (let tab of tabs){
-  channelList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="get1stList('${siteNameVar}','${tab[1]}','${tab[0]}')">${tab[1]}</button>`;
-}
-get1stList(siteNameVar,tabs[0][1],tabs[0][0]);
+//    CREATE URL LIST & SEARCH LIST FOR NEWSBEITER
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-for (let tab of faqB){
-  searchList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="getFAQSearchResults('${tab[0]}','${tab[1]}','${tab[2]}')">${tab[2]}</button>`;
-}  
-searchList.innerHTML+='<input type="text" id="search-term" class="form-control my-2">';
-for (let tab of searchSitesB){
-  searchList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="get1stSearchResults('${tab[0]}','${tab[1]}')">${tab[1]}</button>`;
-}
+if (window.location.href.indexOf('evenbeiter.github.io')!==-1){
+  ping();
+  for (let tab of allSites1){
+    urlList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="createChannelList(${tab[0]},'${tab[0]}','${tab[1]}')">${tab[1]}</button>`;
+  }
+  urlList.innerHTML+='<hr>';
+  for (let tab of allSites2){
+    urlList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="createChannelList(${tab[0]},'${tab[0]}','${tab[1]}')">${tab[1]}</button>`;
+  }
+  urlList.innerHTML+='<hr>';
+  for (let tab of msnVideo){
+    urlList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="get1stList('msnVideo','${tab[1]}','${tab[0]}')">${tab[1]}</button>`;
+  }
+  for (let tab of msnChannelVideo){
+    urlList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="get1stList('msnChannelVideo','${tab[1]}','${tab[0]}')">${tab[1]}</button>`;
+  }
+  for (let tab of otherVideo){
+    urlList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="get1stList('${tab[0]}','${tab[1]}','')">${tab[1]}</button>`;
+  }
+  options.style.display='block';
+  topdiv.style.display='none';
+  createSearchListDiv(faq,searchSites);
+  openUrlList();
+  
+//    CREATE CHANNEL, SEARCH & URL LIST FOR BOOKMARKLET
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+} else if (siteNameVar!==''){
+  createChannelList(tabs,siteNameVar,tabs[0][1])
+  createSearchListDiv(faqB,searchSitesB);
+  createUrlListDiv4Bml([...allSitesB,...otherVideoB]);
 } else {
-  openUrlList()
-}
-
-for (let tab of allSitesB){
-  urlList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="openUrl('${tab[4]}')">${tab[1]}</button>`;
-}
-urlList.innerHTML+='<hr>';
-for (let tab of otherVideoB){
-  urlList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="openUrl('${tab[4]}')">${tab[1]}</button>`;
+  chennelList.innerHTML='';
+  searchList.innerHTML='';
+  createUrlListDiv4Bml([...allSitesB,...otherVideoB]);
+  openUrlList();
 }
