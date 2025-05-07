a27cdfde14e6d7773bedd72905b81e723e3e16a0
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-05-07 12:42:23 +0800

    Update news.js

--- js/news.js
+++ js/news.js
@@ -350,10 +350,10 @@ async function ping(){
 function createChannelList(site,siteName,top){
   channelList.innerHTML='';
   for (let tab of site){
-    channelList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="get1stList('${siteName}','【${top}】${tab[1]}','${tab[0]}')">${tab[1]}</button>`;
+    channelList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="get1stList('${siteName}','${top} | ${tab[1]}','${tab[0]}')">${tab[1]}</button>`;
   }
   openChannelList();
-  get1stList(siteName,'【'+top+'】'+site[0][1],site[0][0]);
+  get1stList(siteName, top+' | '+site[0][1],site[0][0]);
 }
 
 function createSearchListDiv(faqList,searchSiteList){
