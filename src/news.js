2f7a1073b9d0e5a247096f04b383936458dd5538
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-05-09 14:20:22 +0800

    Update news.js

--- src/news.js
+++ src/news.js
@@ -114,7 +114,7 @@ document.documentElement.innerHTML=`
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<link href="src/news.css" rel="stylesheet">
+<link href="https://evenbeiter.github.io/newsbeiter/src/news.css" rel="stylesheet">
 <title>${docTitle}</title>
 </head>
 <body>
