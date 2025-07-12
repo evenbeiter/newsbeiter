6b7157b6193fa143b9a99f79df756b0258a51a0a
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-07-12 15:33:30 +0800

    Update news.js

--- src/news.js
+++ src/news.js
@@ -345,12 +345,16 @@ async function getContent(siteName,clickedId,id){
             var biiFlourish=cEl.querySelectorAll('.bii-flourish'); for (let b of biiFlourish){b.classList.add('p-0')};
             var blkIframe=cEl.querySelectorAll('iframe[id^="bii-flourish"]');
             for (let b of blkIframe){
+              b.parentElement.dataset.src=b.src.replace('https://flo.uri.sh/','').replace('/embed','');
+              b.parentElement.style.aspectRatio=1.78;
+              b.parentElement.style.width='100%';
               b.outerHTML+=`
               <p class="text-end"><button type="button" class="btn btn-light" onclick="showOverlay('blkIframe','${b.src}')">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-fullscreen" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707m4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707m0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707m-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707"/>
                 </svg>
               </button></p>`;
+              b.remove();
               }
           }catch{}
         }
