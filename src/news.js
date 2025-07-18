22538989bcc56db6ce1ec32deb96faf5b57745d9
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-07-18 11:03:55 +0800

    Update news.js

--- src/news.js
+++ src/news.js
@@ -1660,7 +1660,7 @@ async function nbGetList(siteName,t){
 async function nbGetContent(id){
   try{const res = await fetch(preStr+id);const str=await res.text();
   var parser=new DOMParser();var doc=parser.parseFromString(str, "text/html");console.log(doc.querySelectorAll('.col-xs-12.col-sm-9')[1]);
-  html = '<p class="time">'+(doc.querySelector('.article-date-content')?.innerText??'')+'</p>'+(doc.querySelector('main')?.innerHTML.replace(/ class="[\s\S]*?"/g,'') + '<p class="text-end"><a href="' + id + '" target="_blank">Share</a></p><br>';
+  html = '<p class="time">'+(doc.querySelector('.article-date-content')?.innerText??'')+'</p>'+(doc.querySelector('main')?.innerHTML.replace(/ class="[\s\S]*?"/g,'')??'') + '<p class="text-end"><a href="' + id + '" target="_blank">Share</a></p><br>';
   }catch{html='<p><a href="' + id + '" target="_blank">繼續閱讀</a></p><br>'}
   return html;
 }
