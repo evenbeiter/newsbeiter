67ac955ed0b487320fb4bf32d553afdd1261866a
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-10-08 15:29:46 +0800

    new podcast.html

--- src/global.js
+++ src/global.js
@@ -90,7 +90,7 @@ async function getList(siteName,t){
     var all=document.querySelectorAll('.t-tl');
     getTranslation(all);
   }
-  if (window.location.href.slice(-7)==='podcast'){toc.style.display='block'};
+  // if (window.location.href.slice(-7)==='podcast'){toc.style.display='block'};
 }
 
 async function getContent(siteName,clickedId,id){
