5b3ac759557b1730e6ee9bfcd0c29940cd8cf028
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-10-10 06:23:34 +0800

    Update global.js

--- src/global.js
+++ src/global.js
@@ -322,6 +322,7 @@ function startLazyTranslation(containerElement) {
     for (const entry of entries) {
       if (!entry.isIntersecting) continue;
       const el = entry.target;if (shouldIgnore(el)) {observer.unobserve(el);continue;}
+      if (el.innerText==='' || cnTest(el.innerText)===true) return;
       const rawText = el.outerHTML.trim();if (!rawText) {observer.unobserve(el);continue;}
       //      const rawText = getTextOnly(el).trim();if (!rawText) {observer.unobserve(el);continue;}
 
@@ -604,3 +605,4 @@ const svgDelete = `
     <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
   </svg>
 `;
+
