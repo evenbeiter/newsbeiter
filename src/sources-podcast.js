2c42f9abc4182b8165cb686ad8136c8ba7259c04
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-10-10 11:38:59 +0800

    Update sources-podcast.js

--- src/sources-podcast.js
+++ src/sources-podcast.js
@@ -1,3 +1,4 @@
-const pd=[['2766','WSJ Minute Briefing'],['240','FT News Briefing'],['2481','The Indicator'],['20546','Squawk Pod'],['14291','Squawk on the Street'],['2301177','WSJ Take on the Week'],['652','WSJ Whats News'],['693','AEE'],['2243170','AEEBE'],['2062','BEP'],['1443','TED Business'],['3675','The English We Speak'],['2320331','BBCLE from the News'],['5443','English Stories'],,['4025','Culips'],['19418','Real English'],['4624','Luke'];
+const pd=[['2766','WSJ Minute Briefing'],['240','FT News Briefing'],['2481','The Indicator'],['20546','Squawk Pod'],['14291','Squawk on the Street'],['2301177','WSJ Take on the Week'],['652','WSJ Whats News'],['693','AEE'],['2243170','AEEBE'],['2062','BEP'],['1443','TED Business'],['3675','The English We Speak'],['2320331','BBCLE from the News'],['5443','English Stories'],['4025','Culips'],['19418','Real English'],['4624','Luke']];
 
 const allSites=[['pd','Podcast',pd,'','']];
+
