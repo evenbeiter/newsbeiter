28888e16b7f6e0e09b666a32a6814e654a59e675
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-10-10 11:35:09 +0800

    Update sources-podcast.js

--- src/sources-podcast.js
+++ src/sources-podcast.js
@@ -1,23 +1,3 @@
-const pd=[['2766','WSJ Minute Briefing'],['240','FT News Briefing'],['2481','The Indicator'],['20546','Squawk Pod'],['14291','Squawk on the Street'],['2301177','WSJ Take on the Week'],['652','WSJ Whats News'],['693','AEE'],['2243170','AEEBE'],['2062','BEP'],['1443','TED Business']];
+const pd=[['2766','WSJ Minute Briefing'],['240','FT News Briefing'],['2481','The Indicator'],['20546','Squawk Pod'],['14291','Squawk on the Street'],['2301177','WSJ Take on the Week'],['652','WSJ Whats News'],['693','AEE'],['2243170','AEEBE'],['2062','BEP'],['1443','TED Business'],['3675','The English We Speak'],['2320331','BBCLE from the News'],['5443','English Stories'],,['4025','Culips'],['19418','Real English'],['4624','Luke'];
 
 const allSites=[['pd','Podcast',pd,'','']];
-
-// https://backend.podscribe.ai/api/episode/139588851/transcription?transcriptVersionReqId=9d4e2d57-d6e9-4ce6-81b2-4ce555b98bae
-
-// https://backend.podscribe.ai/api/episode?id=118796362
-
-// https://jfe93e.s3.amazonaws.com/-2767374462171352103/6241111523910474747/s93290-US-177s-1759788904.mp3
-
-// https://podscribe-transcript.s3.amazonaws.com/transcripts/9d4e2d57-d6e9-4ce6-81b2-4ce555b98bae.json
-
-
-// https://backend.podscribe.ai/api/episode?id=118796362&includeAds=true&includeOriginal=false&rssUrl=&showAutoLabel=NaN
-
-// https://jfe93e.s3.amazonaws.com/-2767374462171352103/6241111523910474747/s93290-US-177s-1759788904.mp3
-
-// https://podscribe-transcript.s3.amazonaws.com/transcripts/9d4e2d57-d6e9-4ce6-81b2-4ce555b98bae.json
-
-// https://podscribe-transcript.s3.amazonaws.com/transcripts/9d4e2d57-d6e9-4ce6-81b2-4ce555b98bae.json
-
-// https://backend.podscribe.ai/api/series/v2/2766/episodes?archivedIncludedFilter=false&count=50&episodeTitle=&seriesId=2766
-// https://backend.podscribe.ai/api/series/v2/693/episodes?archivedIncludedFilter=false&count=50&cursor=2025-07-29T05:00:00.000Z&episodeTitle=&seriesId=693
