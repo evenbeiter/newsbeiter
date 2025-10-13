cfa3a53f7e3e6bdd95c5e65408acd40f8126b44b
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-10-13 15:37:12 +0800

    add eu content

--- src/functions-podcast.js
+++ src/functions-podcast.js
@@ -1,3 +1,123 @@
+//    EUDICT
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+async function euGetList(siteName,t) {
+
+  try {
+  const res = await fetch(preStr+'https://mob2015.kekenet.com/keke/mobile/index.php', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload)
+  });
+  const data = await res.json();
+
+  if (data.Code !== 200) throw new Error(data.Msg || "API 返回錯誤");
+
+  const contentData = data.IsDecode == 1 ? await decryptAES(data.Data) : data.Data;
+
+  for (let h of contentData.list){
+    items.push([h.id,h.title,h.updatetime,h.mp3len])
+  }
+  for (let h of items){
+    html+=`<p class="title fs12" onclick="keGetContent('${h[0]}')">${s2t(h[1])}<br><span class="time">${h[2]} | </span><span class="fs10 fw-bold">${h[3]}</span></p><div id="${h[0]}" class="content">
+    <div class="pt-2 sepia">
+      <table class="table table-auto fs11 p-0 sepia">
+        <tbody id="lines-${h[0]}"></tbody>
+      </table>
+    </div>
+    </div><hr>`;
+  }
+
+  }catch{html='<p>尚無內容</p>'}
+
+  ap.style.display='block';vp.style.display='none';ap.src='';vp.src='';
+
+  return html;
+}
+
+async function euGetContent(id){
+
+  const cEl=document.getElementById(id);
+
+  if (cEl.style.display=='none' || cEl.style.display==''){
+    cEl.style.display='block';
+    loading.style.display='block';
+
+    if (cEl.innerText.length>10) return; // already got transcription in cEl
+
+    try{
+
+    const res = await fetch('https://dict.eudic.net/webting/desktopplay?id=';
+    const str = await res.text();
+    const parser=new DOMParser();const doc=parser.parseFromString(str, "text/html");
+
+    const scripts = doc.querySelectorAll('script');
+    const raw = Array.from(scripts).filter(s=>s.textContent.includes('translate'));
+
+    const getVarValue = (text, varName) => {
+      const regex = new RegExp(`(?:var|let|const)\\s+${varName}\\s*=\\s*([^;]+)`);
+      const match = text.match(regex);
+      if (!match) return null;
+
+      let value = match[1].trim();
+      // 嘗試移除引號
+      if ((value.startsWith('"') && value.endsWith('"')) ||
+          (value.startsWith("'") && value.endsWith("'"))) {
+        value = value.slice(1, -1);
+      }
+      return value;
+    };
+
+    const contentData = JSON.parse(getVarValue(raw[0].innerText, "translate"));
+    const rawLrc = contentData.subtitles;
+
+    let mediaSrc = 'https://api.frdic.com/api/v3/media/mp3/'+id;
+    if (doc.querySelectorAll('audio').length!==0) {
+      media=ap;
+      ap.src= mediaSrc;vp.src='';
+      ap.style.display='block';vp.style.display='none';
+    } else if (doc.querySelectorAll('video').length!==0) {
+      media=vp;
+      vp.src= mediaSrc;ap.src='';
+      vp.style.display='block';ap.style.display='none';
+    }
+
+    let ts=[];
+    if (rawLrc.length>0){
+      for (let s of rawLrc){
+        ts.push({
+          startTime: cvtMMSS2S(s.timestamps[0].slice(1,-1)),
+          sentence: `${s.origintext}<br>${s2t(s.translation)}`
+        });
+      }
+    } 
+
+    if (contentData.phrasehints.length>0) {
+      for (let s of contentData.phrasehints){
+        ts.push({
+          startTime: null,
+          sentence: `${s.phrase}<br>${s2t(s.exp)}<br>${s.context}`
+        });
+      }
+    }
+
+    getLinesTable(ts,id,true);
+    loading.style.display='none';
+
+    } catch {cEl.innerHTML+=`<p>尚未提供文稿</p>`}
+
+  } else {
+    cEl.style.display='none';
+  }
+}
+
+function cvtMMSS2S(text){
+  const ms = text.split(':');
+  return ms[0]*60 + ms[1]*1;
+}
+
+
 //    KEKENET
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -86,14 +206,14 @@ async function keGetContent(id){
     const contentData = data.IsDecode == 1 ? await decryptAES(data.Data) : data.Data;
     const rawLrc = contentData.content;
 
-    const mediaSrc = 'https://k6.kekenet.com/'+contentData.playurl;
+    let mediaSrc = 'https://k6.kekenet.com/'+contentData.playurl;
     if (mediaSrc.endsWith('.mp3')) {
       media=ap;
-      ap.src= 'https://k6.kekenet.com/'+contentData.playurl;vp.src='';
+      ap.src= mediaSrc;vp.src='';
       ap.style.display='block';vp.style.display='none';
     } else {
       media=vp;
-      vp.src= 'https://k6.kekenet.com/'+contentData.playurl;ap.src='';
+      vp.src= mediaSrc;ap.src='';
       vp.style.display='block';ap.style.display='none';
     }
 
