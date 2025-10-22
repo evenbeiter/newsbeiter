7534f0ffe04e4ed5cd208e212b90cc11014a277f
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-10-22 18:06:56 +0800

    Update fetch-podcast.js

--- src/fetch-podcast.js
+++ src/fetch-podcast.js
@@ -328,8 +328,8 @@ async function ytbGetList(siteName,t){
 
   try{
     const res=await fetch(preStr+encodeURIComponent('https://www.youtube.com/playlist?list='+t));
-    const str=await res.text();
-    const data=JSON.parse(str.match(/var\s+ytInitialData\s*=\s*([\s\S]*?);<\/script>/)?.[1]).contents.twoColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents[0].itemSectionRenderer.contents[0].playlistVideoListRenderer.contents;
+    const str=await res.text();console.log(str);
+    const data=JSON.parse(str.match(/var\s+ytInitialData\s*=\s*([\s\S]*?);<\/script>/)?.[1]).contents.twoColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents[0].itemSectionRenderer.contents[0].playlistVideoListRenderer.contents;console.log(data);
     for (let d of data){
       items.push([d.playlistVideoRenderer.videoId,d.playlistVideoRenderer.title.runs[0].text,d.playlistVideoRenderer.lengthText.simpleText]);
     }
@@ -819,3 +819,4 @@ const loop=`
   <path d="M9 5.5a.5.5 0 0 0-.854-.354l-1.75 1.75a.5.5 0 1 0 .708.708L8 6.707V10.5a.5.5 0 0 0 1 0z"/>
 </svg>
 `;
+
